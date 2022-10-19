11c079f2953c73891f5d34d12472f5c8c436cbaf
HJH1122 <ktorn21@naver.com>
2022-10-19 18:17:57 +0900

    함수 호출방식, 인수, 반환값, 내장함수, 모듈, 성적처리 프로그램v5

--- 23func.py
+++ 23func.py
@@ -160,17 +160,18 @@ checkCredit(code)
 # ex) 60갑자를 출력해주는 프로그램을 함수로 작성
 # 함수명 : checkChinaZodiac(년도)
 
-BASE = 1444
-ten = ('값', '을', '병', '정', '무', '기', '경', '신', '임', '계')
-twelve = ('자', '축', '인', '묘', '진', '사', '오', '미', '신', '유', '술', '해')
-animal = ('쥐', '소', '호랑이', '토끼', '용', '뱀', '말', '양', '원숭이', '닭', '개', '돼지')
+def checkChinaZodiac(year):
+    baseYear = 1444
+    ten = ('갑', '을', '병', '정', '무', '기', '경', '신', '임', '계')
+    twelve = ('자', '축', '인', '묘', '진', '사', '오', '미', '신', '유', '술', '해')
+    animal = ('쥐', '소', '호랑이', '토끼', '용', '뱀', '말', '양', '원숭이', '닭', '개', '돼지')
 
-year = int(input('연도 입력 :'))
-i1 = (year - BASE) % 10
-i2 = (year - BASE) % 12
-print('%d년은 %s년(%s의 해) 입니다' % (year, ten[i1]+twelve[i2], animal[i2]))
+    t10idx = (year - baseYear) % 10
+    t12idx = (year - baseYear) % 12
+    print(f'{year}년은 {ten[t10idx]}{twelve[t12idx]}년'f'({animal[t12idx]}의 해) 입니다')
 
-
+year = int(input('년도?'))
+checkChinaZodiac(year)
 
 
 
---
+++ bigdata.py
@@ -0,0 +1,20 @@
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
